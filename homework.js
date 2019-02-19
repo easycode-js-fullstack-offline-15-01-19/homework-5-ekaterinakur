48c4a1da1e33bb560096ef94e11b661b282b73a8
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-02-19 13:50:33 +0200

    With corrections: tasks 6.1, 7.2, setAttribute -> classList.add

--- homework.js
+++ homework.js
@@ -29,8 +29,8 @@ if (document.body.firstElementChild.children) {
 //  1. Создать функцию, которая принимает два элемента. Функция проверяет, является ли первый элемент родителем для второго:
 
 function isParent(parent, child) {
-  return parent === child.parentElement ? true : false; 
-};
+  return child.closest(parent.tagName) === parent;
+}
 
 isParent(document.body.children[0], document.querySelector('mark'));  // false
 isParent(document.querySelector('p'), document.querySelector('mark')); // true
@@ -60,16 +60,22 @@ console.log(paragraph.textContent);
  
 //  2. Создать функцию, которая принимает в качестве аргумента узел DOM и возвращает информацию (в виде объекта) о типе узла, об имени узла и о количестве дочерних узлов (если детей нет - 0)
 function typeNode(node) {
-  let obj = {};
   let qsNode = document.querySelector(node);
-  let childNumber = Array.from(document.querySelector(node).childNodes);
+  return { type: qsNode.nodeType, name: qsNode.nodeName, children: qsNode.children.length };
+}
 
-  obj.type = qsNode.nodeType;
-  obj.name = qsNode.nodeName;
-  obj.childNum = childNumber.length;
+//  до правок ---------
+// function typeNode(node) {
+//   let obj = {};
+//   let qsNode = document.querySelector(node);
+//   let childNumber = Array.from(document.querySelector(node).childNodes);
 
-  return obj;
-};
+//   obj.type = qsNode.nodeType;
+//   obj.name = qsNode.nodeName;
+//   obj.childNum = childNumber.length;
+
+//   return obj;
+// }
 
 //  3. Получить массив, который состоит из текстового содержимого ссылок внутри списка: 
 function getTextFromUl() {
@@ -81,7 +87,7 @@ function getTextFromUl() {
   };
 
   return arr;
-};
+}
 
 //  4. В параграфе заменить все дочерние текстовые узлы на “-text-” (вложенные теги должны остаться).
 let paragraph4 = document.querySelector('p');
@@ -96,7 +102,7 @@ for (let i = 0; i < child.length; i++){
 //  Презентация 7. Слайд 11 ------------------------------
 //  1. Найти в коде список ul и добавить класс “list”
 let list = document.querySelector('ul');
-list.setAttribute('class', 'list');
+list.classList.add('list');
 
 //  2. Найти в коде ссылку, находящуюся после списка ul, и добавить id=link
 let link2 = document.querySelector('span');
@@ -108,7 +114,7 @@ let classLi = document.getElementsByTagName('li');
 
 for (let i = 0; i < classLi.length; i++) {
   if (i % 2 == false) {
-    classLi[i].setAttribute('class', 'item');
+    classLi[i].classList.add('item');
   }
 };
 
@@ -116,7 +122,7 @@ for (let i = 0; i < classLi.length; i++) {
 let classA = document.querySelectorAll('a');
 
 for (let i = 0; i < classA.length; i++) {
-  classA[i].setAttribute('class', 'custom-link');
+  classA[i].classList.add('custom-link');
 };
 
 //  Презентация 7. Слайд 17 ------------------------------
@@ -125,7 +131,7 @@ let ulNewLi = document.querySelector('ul');
 
 for (let i = 0; i < 4; i++) {
   let newli = document.createElement('li');
-  newli.setAttribute('class', 'new-item');
+  newli.classList.add('new-item');
   newli.textContent = 'Item' + (ulNewLi.children.length + 1);
   ulNewLi.appendChild(newli);
 };
@@ -152,7 +158,7 @@ document.body.insertAdjacentElement('afterbegin', myImg);
 // 4. Найти на странице элемент mark, добавить в конец содержимого текст “green” и на элемент установить класс green
 let myMark = document.querySelector('mark');
 myMark.insertAdjacentText('beforeend', ' green');
-myMark.setAttribute('class', 'green');
+myMark.classList.add('green');
 
 // 5. Отсортировать li внутри списка в обратном порядке (по тексту внутри)
 let arrLi = document.querySelector('ul').children;
